d24d41f635ec7a911c7a7ee948761e716d63c2de
Abhinav Das <abhinav.das1997@gmail.com>
2018-04-18 19:36:22 -0500

    print pred matrices

--- Code/hw8.py
+++ Code/hw8.py
@@ -12,7 +12,7 @@ def import_data():
 	update_coords = pd.read_csv('../Data/SupplementaryAndSampleData/UpdateOrderCoordinates.csv',sep=',')
 	initial_params = pd.read_csv('../Data/SupplementaryAndSampleData/InitialParametersModel.csv',sep=',',header=None)
 	initial_params = np.array(initial_params)
-	return train_images[0:19,:],train_labels[0:19],update_coords,initial_params
+	return train_images[0:20,:],update_coords,initial_params
 
 def binarize(train_images):
 	train_images[train_images<128] = -1
@@ -45,7 +45,7 @@ def display_all(train_images):
 		display_image(train_images,i)
 
 
-def save_all(images,description):
+def save_images(images,description):
 	for i in range(len(images)):
 		image_out = np.zeros((images.shape[0],images.shape[1]))
 		image_out[images==-1] = 0
@@ -54,7 +54,10 @@ def save_all(images,description):
 		image_out = image_out.astype('uint8')
 		im = Image.fromarray(image_out[i,:,:],mode='L')
 		im.save('../Output/Image_'+str(i)+'_'+description+'.png')
-
+		
+def save_predictions(predictions):
+	for i in range(len(predictions)):
+		np.savetxt('../Output/Image_'+str(i)+'_Prediction',predictions[i],delimiter='\t',fmt='%d')
 
 def mean_field_inference(update_coords,train_images,pi,theta_xh,theta_hh,num_iter,img_num):
 	
@@ -87,12 +90,19 @@ def reconstruct_image(pi):
 	
 def mean_field_wrapper(update_coords,train_images,initial_params,theta_xh,theta_hh,num_iter):
 	denoised_images = np.zeros(train_images.shape)
+	predictions = []
 	for i in range(len(train_images)):
 		pi = mean_field_inference(update_coords,train_images,initial_params,theta_xh,theta_hh,num_iter,i)
 		image = reconstruct_image(pi)
 		image = image.flatten()
 		denoised_images[i,:] = image
-	return denoised_images
+		pred = pi
+		pred[pred<0.5] = 0
+		pred[pred>=0.5] = 1
+		predictions.append(pred)
+		
+
+	return denoised_images,predictions
 	
 
 
@@ -101,12 +111,13 @@ def main():
 	theta_xh = 2
 	theta_hh = 0.8
 	num_iter = 10
-	train_images, train_labels, update_coords,initial_params = import_data()
+	train_images,update_coords,initial_params = import_data()
 	train_images = binarize(train_images)
-	save_all(train_images,'original')
+	save_images(train_images,'original')
 	train_images = add_noise(train_images)
-	save_all(train_images,'noisy')
-	denoised_images = mean_field_wrapper(update_coords,train_images,initial_params,theta_xh,theta_hh,num_iter)
-	save_all(denoised_images,'denoised')
+	save_images(train_images,'noisy')
+	denoised_images, predictions = mean_field_wrapper(update_coords,train_images,initial_params,theta_xh,theta_hh,num_iter)
+	save_images(denoised_images,'denoised')
+	save_predictions(predictions)
 
 main()
